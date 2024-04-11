eae75e94ce3f2088fa2f8fd5ae0b703ccc2d4d4a
dino.shin <troas96@naver.com>
2024-04-11 15:27:28 +0900

    refactor: QueryStringParser separator 추가
    Co-authored-by: HyewonKkang <gpffps369@gmail.com>

--- src/main/java/utils/QueryStringParser.java
+++ src/main/java/utils/QueryStringParser.java
@@ -6,21 +6,24 @@ import java.util.stream.Collectors;
 
 public class QueryStringParser {
 
-    private QueryStringParser() {
-    }
+    private static final String QUERY_STRING_SEPARATOR = "\\?";
 
     private static final String PARAMETER_SEPARATOR = "&";
     private static final int ZERO = 0;
     private static final int ONE = 1;
     private static final String NAME_VALUE_SEPARATOR = "=";
 
+    private QueryStringParser() {
+    }
+
     public static Map<String, String> parse(final String query) {
         if (query == null || query.isEmpty()) {
             return Map.of();
         }
-        final String[] params = query.split(PARAMETER_SEPARATOR);
+
+        final String[] params = query.split(QUERY_STRING_SEPARATOR)[ONE].split(PARAMETER_SEPARATOR);
         return Arrays.stream(params)
                 .map(param -> param.split(NAME_VALUE_SEPARATOR))
-                .collect(Collectors.toMap(pair -> pair[ZERO], pair -> pair[ONE], (a, b) -> b));
+                .collect(Collectors.toMap(pair -> pair[ZERO], pair -> pair[ONE]));
     }
 }
