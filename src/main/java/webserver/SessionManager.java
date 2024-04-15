e700aea89f46df4a2179991c5df3ed711929d603
dino.shin <troas96@naver.com>
2024-04-15 23:42:34 +0900

    feat: 세션 구현

---
+++ src/main/java/webserver/SessionManager.java
@@ -0,0 +1,32 @@
+package webserver;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
+    private static final SessionManager instance = new SessionManager();
+
+    private SessionManager() {
+    }
+
+    public static SessionManager getInstance() {
+        return instance;
+    }
+
+    public Session add(final String sessionId) {
+        Session session = new Session(sessionId);
+        SESSIONS.put(sessionId, session);
+        return session;
+    }
+
+    public Session findSession(final String id) {
+        return SESSIONS.get(id);
+    }
+
+    public void remove(final String id) {
+        SESSIONS.remove(id);
+    }
+}
+
---
+++ src/test/java/webserver/SessionManagerTest.java
@@ -0,0 +1,38 @@
+package webserver;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+    private SessionManager sessionManager;
+
+    @BeforeEach
+    public void setUp() {
+        sessionManager = SessionManager.getInstance();
+    }
+
+    @Test
+    void 새로운_세션을_추가할_수_있다() {
+        // given
+        final String sessionId = "uniqueSessionId";
+
+        // when
+        final Session session = sessionManager.add(sessionId);
+
+        assertThat(sessionManager.findSession(sessionId)).isEqualTo(session);
+    }
+
+    @Test
+    void 세션을_삭제할_수_있다() {
+        // given
+        final String sessionId = "uniqueSessionId";
+        sessionManager.add(sessionId);
+
+        // when
+        sessionManager.remove(sessionId);
+        assertThat(sessionManager.findSession(sessionId)).isNull();
+    }
+}
