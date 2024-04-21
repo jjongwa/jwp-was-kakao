f06af4704051612c451dc9511339b1e99e7cfa51
dino.shin <troas96@naver.com>
2024-04-21 23:08:19 +0900

    refactor: Cookie에서 path 값을 따로 관리하도록 변경

--- src/main/java/webserver/HttpCookie.java
+++ src/main/java/webserver/HttpCookie.java
@@ -12,20 +12,23 @@ public class HttpCookie {
     private static final String SPLIT_REGEX = ": ";
     private static final String ELEMENT_SPLIT_REGEX = "; ";
     private static final String KEY_VALUE_SPLIT_REGEX = "=";
-    private static final String COOKIE_PATH_SUFFIX = "; Path=/";
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
     private static final Supplier<String> EMPTY_STRING = ""::toString;
     private static final String REQUEST_COOKIE_KEY = "Cookie";
+    private static final String DEFAULT_PATH = "/";
+    private static final String PATH_KEY = "Path";
 
     private final Map<String, String> elements;
+    private final String path;
 
-    private HttpCookie(final Map<String, String> elements) {
+    private HttpCookie(final Map<String, String> elements, final String path) {
         this.elements = elements;
+        this.path = path;
     }
 
     public static HttpCookie createEmptyCookie() {
-        return new HttpCookie(new HashMap<>());
+        return new HttpCookie(new HashMap<>(), DEFAULT_PATH);
     }
 
     public static HttpCookie fromRequestInput(final List<String> headersInput) {
@@ -43,10 +46,20 @@ public class HttpCookie {
         if (cookieHeader == null || cookieHeader.isEmpty()) {
             return HttpCookie.createEmptyCookie();
         }
-        return new HttpCookie(Arrays.stream(cookieHeader.split(ELEMENT_SPLIT_REGEX))
+        final Map<String, String> cookieElements = Arrays.stream(cookieHeader.split(ELEMENT_SPLIT_REGEX))
                 .map(element -> element.split(KEY_VALUE_SPLIT_REGEX))
-                .collect(Collectors.toMap(element -> element[KEY_INDEX], element -> element[VALUE_INDEX]))
-        );
+                .filter(element -> !element[KEY_INDEX].equals(PATH_KEY))
+                .collect(Collectors.toMap(element -> element[KEY_INDEX], element -> element[VALUE_INDEX]));
+        return new HttpCookie(cookieElements, findPath(cookieHeader));
+    }
+
+    private static String findPath(final String cookieHeader) {
+        return Arrays.stream(cookieHeader.split(ELEMENT_SPLIT_REGEX))
+                .map(element -> element.split(KEY_VALUE_SPLIT_REGEX))
+                .filter(element -> element[KEY_INDEX].equals(PATH_KEY))
+                .findFirst()
+                .map(element -> element[VALUE_INDEX])
+                .orElse(DEFAULT_PATH);
     }
 
     public void addElement(final String key, final String value) {
@@ -68,7 +81,7 @@ public class HttpCookie {
         if (key == null || key.isEmpty() || !elements.containsKey(key)) {
             return null;
         }
-        return elements.get(key) + COOKIE_PATH_SUFFIX;
+        return elements.get(key) + ELEMENT_SPLIT_REGEX + PATH_KEY + KEY_VALUE_SPLIT_REGEX + path;
     }
 
     public Map<String, String> getElements() {
