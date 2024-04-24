edbe792e3255299bfaf26d3a42f23810ecd6c98f
dino.shin <troas96@naver.com>
2024-04-24 23:13:42 +0900

    feat: thread pool 적용

--- src/main/java/webserver/WebApplicationServer.java
+++ src/main/java/webserver/WebApplicationServer.java
@@ -1,8 +1,5 @@
 package webserver;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.controller.DefaultController;
@@ -10,9 +7,19 @@ import webserver.controller.LoginController;
 import webserver.controller.RegisterController;
 import webserver.controller.UserListController;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class WebApplicationServer {
     private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int CORE_POOL_SIZE = 100;
+    private static final int MAXIMUM_POOL_SIZE = 250;
+    private static final long KEEP_ALIVE_TIME = 1L;
+    private static final int WORK_QUEUE_CAPACITY = 100;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -22,11 +29,14 @@ public class WebApplicationServer {
             port = Integer.parseInt(args[0]);
         }
 
-        final RequestMapping requestMapping = new RequestMapping();
-        requestMapping.add("/", new DefaultController());
-        requestMapping.add("/user/create", new RegisterController());
-        requestMapping.add("/user/login", new LoginController());
-        requestMapping.add("/user/list", new UserListController());
+        final RequestMapping requestMapping = initRequestMapping();
+        final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
+                MAXIMUM_POOL_SIZE,
+                KEEP_ALIVE_TIME,
+                TimeUnit.MINUTES,
+                new LinkedBlockingDeque<>(WORK_QUEUE_CAPACITY)
+        );
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -35,9 +45,17 @@ public class WebApplicationServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection, requestMapping));
-                thread.start();
+                threadPoolExecutor.execute(new RequestHandler(connection, requestMapping));
             }
         }
+    }
+
+    private static RequestMapping initRequestMapping() {
+        final RequestMapping requestMapping = new RequestMapping();
+        requestMapping.add("/", new DefaultController());
+        requestMapping.add("/user/create", new RegisterController());
+        requestMapping.add("/user/login", new LoginController());
+        requestMapping.add("/user/list", new UserListController());
+        return requestMapping;
     }
 }
--- src/test/java/webserver/ExecutorsTest.java
+++ src/test/java/webserver/ExecutorsTest.java
@@ -1,21 +1,26 @@
 package webserver;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExecutorsTest {
     private static final Logger logger = LoggerFactory.getLogger(ExecutorsTest.class);
 
-    private static AtomicInteger counter = new AtomicInteger(0);
+    private static final AtomicInteger counter = new AtomicInteger(0);
 
     public static void main(String[] args) throws Exception {
-        ExecutorService es = Executors.newFixedThreadPool(100);
+        final ExecutorService es = Executors.newFixedThreadPool(100);
 
         StopWatch sw = new StopWatch();
         sw.start();
@@ -35,6 +40,31 @@ public class ExecutorsTest {
         es.shutdown();
         es.awaitTermination(100, TimeUnit.SECONDS);
         logger.info("Total Elaspsed: {}", sw.getTotalTimeSeconds());
+    }
+
+    @Test
+    void request_resttemplate() throws InterruptedException {
+        // given
+        final RestTemplate restTemplate = new RestTemplate();
+        final ExecutorService es = Executors.newFixedThreadPool(10);
+        final int tryCount = 250;
+
+        // when
+        IntStream.range(0, tryCount)
+                .parallel()
+                .forEach(i -> {
+                    es.execute(() -> {
+                        final int idx = counter.addAndGet(1);
+                        restTemplate.getForEntity("http://localhost:8080", String.class);
+                        logger.info("request num: {}", idx);
+                    });
+                });
+
+        es.shutdown();
+        es.awaitTermination(100, TimeUnit.SECONDS);
+
+        // then
+        assertThat(counter).hasValue(tryCount);
     }
 }
 
